fd7febe513c379e8f49bced2021b0ec6c7cef6b9
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-04-27 11:21:54 +0530

    map categories and show

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -3,35 +3,53 @@
 import { Badge } from "./ui/badge";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
-interface FilterCarouselProps{
-    value?: string | null;
-    isLoading?: boolean;
-    onSelect?: (value: string) => void;
-    data: {
-        value: string;
-        label: string;
-    }[];
+interface FilterCarouselProps {
+  value?: string | null;
+  isLoading?: boolean;
+  onSelect?: (value: string) => void;
+  data: {
+    value: string;
+    label: string;
+  }[];
 }
 
-export const FilterCarousel = ({value, isLoading, onSelect, data}: FilterCarouselProps) => {
-    return (
-        <div className="relative w-full">
-            <Carousel
-              opts={{
-                align: "start",
-                dragFree: true,
-              }}
-              className="w-full px-12"
+export const FilterCarousel = ({
+  value,
+  isLoading,
+  onSelect,
+  data,
+}: FilterCarouselProps) => {
+  return (
+    <div className="relative w-full">
+      <Carousel
+        opts={{
+          align: "start",
+          dragFree: true,
+        }}
+        className="w-full px-12"
+      >
+        <CarouselContent className="-ml-3">
+          <CarouselItem className="pl-3 basis-auto">
+            <Badge
+              variant={value === null ? "default" : "secondary"}
+              className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
             >
-                <CarouselContent className="-ml-3">
-                    <CarouselItem>
-                        <Badge>
-                            All
-                        </Badge>
-                    </CarouselItem>
-                </CarouselContent >
-
-            </Carousel>
-        </div>
-    )
-}
+              All
+            </Badge>
+          </CarouselItem>
+          {!isLoading &&
+            data.map((item) => (
+              <CarouselItem key={item.value} className="pl-3 basis-auto">
+                <Badge
+                  variant={value === item.value ? "default" : "secondary"}
+                  className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                >
+                  {item.label}
+                </Badge>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+};
