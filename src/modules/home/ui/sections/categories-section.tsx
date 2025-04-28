40b5025d1f1293ff10e7b289d00c5561e508d7ea
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-04-28 16:07:23 +0530

    router to category url

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -1,29 +1,46 @@
-'use client';
+"use client";
 
 import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 import { FilterCarousel } from "@/components/filter-carousel";
+import { useRouter } from "next/navigation";
 
-interface CategoriesSectionProps{
-    categoryId?: string;
+interface CategoriesSectionProps {
+  categoryId?: string;
 }
 
-export const CategoriesSection = ({categoryId}: CategoriesSectionProps) => {
-    return(
-        <Suspense fallback={<p>Loading...</p>}>
-            <ErrorBoundary fallback={<p>Error</p>}>
-                <CategoriesSectionSuspense categoryId={categoryId} />
-            </ErrorBoundary>
-        </Suspense>
-    )
-}
+export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+  return (
+    <Suspense fallback={<CategoriesSkelton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
 
-const CategoriesSectionSuspense = ({categoryId}: CategoriesSectionProps) => {
-    const [categories] = trpc.categories.getMany.useSuspenseQuery();
-    const data = categories.map(({name, id}) => ({
-        value: id,
-        label: name,
-    }))
-    return <FilterCarousel onSelect={(x) => {console.log(x)}} data={data} value={categoryId}/>
-}
+const CategoriesSkelton = () => {
+  return <FilterCarousel isLoading onSelect={() => {}} data={[]} />;
+};
+
+const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
+  const router = useRouter();
+  const [categories] = trpc.categories.getMany.useSuspenseQuery();
+  const data = categories.map(({ name, id }) => ({
+    value: id,
+    label: name,
+  }));
+
+  const onSelect = (value: string | null) => {
+    const url = new URL(window.location.href);
+    if (value) {
+      url.searchParams.set("categoryId", value);
+    } else {
+      url.searchParams.delete("categoryId");
+    }
+    router.push(url.toString());
+  };
+  
+  return <FilterCarousel onSelect={onSelect} data={data} value={categoryId} />;
+};
