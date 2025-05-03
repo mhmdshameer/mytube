81ca5f47b2651049f7cfbe6169a366925174d10e
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-03 20:26:49 +0530

    create studio navbar

---
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -0,0 +1,32 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+import { SearchInput } from "./search-input";
+import { AuthButton } from "@/modules/auth/ui/components/auth-button";
+
+export const StudioNavbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+      {/* Menu and Logo */}
+      <div className="flex items-center gap-4 w-full">
+        <div className="flex items-center flex-shrink-0 gap-2">
+          <SidebarTrigger />
+          <Link href="/studio">
+            <div className="flex items-center gap-1 cursor-pointer">
+              <Image src="/logo.svg" alt="logo" width={32} height={32} />
+              <p className="text-xl font-semibold tracking-tight">Studio</p>
+            </div>
+          </Link>
+        </div>
+
+        {/* Spacer */}
+        <div className="flex-1"></div>
+
+        {/* Auth Button */}
+        <div className="flex-shrink-0 items-center flex gap-4">
+            <AuthButton/>
+        </div>
+      </div>
+    </nav>
+  );
+};
--- src/modules/studio/ui/layouts/studio-layouts.tsx
+++ src/modules/studio/ui/layouts/studio-layouts.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { SidebarProvider } from "@/components/ui/sidebar";
-import HomeNavbar from "@/modules/home/ui/components/home-navbar";
 import { HomeSidebar } from "@/modules/home/ui/components/home-sidebar";
+import { StudioNavbar } from "@/modules/studio/ui/components/studio-navbar";
 
 type StudioLayoutProps = {
   children: React.ReactNode;
@@ -12,7 +12,7 @@ export const StudioLayout = ({ children }: StudioLayoutProps) => {
   return (
     <SidebarProvider defaultOpen={true}>
       <div className="min-h-screen" suppressHydrationWarning>
-        <HomeNavbar />
+        <StudioNavbar />
         <div className="flex min-h-[calc(100vh-4rem)] pt-[4rem]">
           <HomeSidebar />
           <main className="flex-1 overflow-y-auto bg-background">{children}</main>
