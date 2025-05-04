d8770093cefd10d2f32755d50bffbced055d1d7a
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-04 16:57:26 +0530

    add some more in schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-// import { relations } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users",{
@@ -11,6 +11,10 @@ export const users = pgTable("users",{
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 },(t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
 
+export const userRelations = relations(users, ({many}) => ({
+    videos: many(videos),
+}))
+
 export const categories = pgTable("categories",{
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull().unique(),
@@ -19,6 +23,10 @@ export const categories = pgTable("categories",{
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 },(t) => [uniqueIndex("name_idx").on(t.name)]);
 
+export const categoryRelations = relations(categories, ({many}) => ({
+    videos: many(videos),
+}))
+
 export const videos = pgTable("videos",{
     id: uuid("id").primaryKey().defaultRandom(),
     title: text("title").notNull(),
@@ -26,14 +34,21 @@ export const videos = pgTable("videos",{
     userId: uuid("user_id").references(() => users.id, {
         onDelete: "cascade",
     }).notNull(),
+    categoryId: uuid("category_id").references(() => categories.id,{
+        onDelete: "set null",
+    }),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })
 
-// export const videoRelations = relations(videos, ({one}) => ({
-//     user: one(users,{
-//         fields: [videos.userId],
-//         references: [users.id],
-//     }),
-// }))
+export const videoRelations = relations(videos, ({one}) => ({
+    user: one(users,{
+        fields: [videos.userId],
+        references: [users.id],
+    }),
+    category: one(categories,{
+        fields: [videos.categoryId],
+        references: [categories.id],
+    }),
+}))
 
