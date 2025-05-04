fcccad06c3d208c4ea6ef6079e97661a23261b72
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-04 15:59:59 +0530

    create button

--- src/modules/studio/ui/components/studio-navbar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -2,6 +2,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import Image from "next/image";
 import Link from "next/link";
 import { AuthButton } from "@/modules/auth/ui/components/auth-button";
+import { StudioUploadModal } from "../studio-upload-modal";
 
 export const StudioNavbar = () => {
   return (
@@ -23,7 +24,8 @@ export const StudioNavbar = () => {
 
         {/* Auth Button */}
         <div className="flex-shrink-0 items-center flex gap-4">
-            <AuthButton/>
+          <StudioUploadModal />
+          <AuthButton />
         </div>
       </div>
     </nav>
---
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+
+export const StudioUploadModal = () => {
+    return (
+        <Button variant="secondary">
+            <Plus/>
+            Create
+        </Button>
+    )
+}
