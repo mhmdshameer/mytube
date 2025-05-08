d4fa913406cbe7dc8afe22b5ef83ba02655bd332
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-08 16:29:22 +0530

    get in real time

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -5,7 +5,12 @@ import { trpc } from "@/trpc/client"
 import { Plus } from "lucide-react"
 
 export const StudioUploadModal = () => {
-    const create = trpc.videos.create.useMutation();
+    const utils = trpc.useUtils();
+    const create = trpc.videos.create.useMutation({
+        onSuccess: () => {
+            utils.studio.getMany.invalidate();
+        },
+    });
     return (
         <Button variant="secondary" onClick={() => create.mutate()}>
             <Plus/>
