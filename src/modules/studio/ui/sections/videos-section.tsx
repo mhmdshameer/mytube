1d4f9909dd8839e5fa068cba3c3e44856ce2f173
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-08 18:13:24 +0530

    show video detatails in table

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,34 +1,81 @@
-'use client'
-import { trpc } from "@/trpc/client"
-import { DEFAULT_LIMIT } from "@/constants"
-import { Suspense } from "react"
-import { ErrorBoundary } from "react-error-boundary"
-import { InfiniteScroll } from "@/components/infinite-scroll"
+"use client";
+import { trpc } from "@/trpc/client";
+import { DEFAULT_LIMIT } from "@/constants";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
 export const VideoSection = () => {
-    return (
-        <Suspense fallback={<p>Loading...</p>}>
-            <ErrorBoundary fallback={<p>Error...</p>}>
-                <VideoSectionSuspense />
-            </ErrorBoundary>
-        </Suspense>
-    )
-}
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideoSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
 
 const VideoSectionSuspense = () => {
-    const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-        limit: DEFAULT_LIMIT,
-    },{
-        getNextPageParam: (lastPage) => lastPage.nextCursor,
-    });
-    return (
-        <div>
-            {JSON.stringify(data)}
-            <InfiniteScroll
-                isManual
-                hasNextPage={query.hasNextPage}
-                isFetchingNextPage={query.isFetchingNextPage}
-                fetchNextPage={query.fetchNextPage}
-            />
-        </div>
-    )
-}
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>Visibility</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Views</TableCell>
+                    <TableCell>Comments</TableCell>
+                    <TableCell>Likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
