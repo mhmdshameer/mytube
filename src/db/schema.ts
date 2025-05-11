243f44155cd747ba31d5a3097ca24d439627baaf
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-11 11:59:29 +0530

    schema update

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { MuxUploaderPause } from "@mux/mux-uploader-react";
 import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
@@ -31,6 +32,12 @@ export const videos = pgTable("videos",{
     id: uuid("id").primaryKey().defaultRandom(),
     title: text("title").notNull(),
     description: text("description"),
+    muxStatus: text("mux_status"),
+    muxAssetId: text("mux_asset_id").unique(),
+    muxUploadId: text("mux_upload_id").unique(),
+    muxPlaybackId: text("mux_playback_id"),
+    muxTrackId: text("mux_track_id").unique(),
+    muxTrackStatus: text("mux_track_status"),
     userId: uuid("user_id").references(() => users.id, {
         onDelete: "cascade",
     }).notNull(),
