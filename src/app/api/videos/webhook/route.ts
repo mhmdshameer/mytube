2c7903dc8599bd7578e0d0c87157240d70219f7a
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-13 16:37:41 +0530

    video preview

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -71,6 +71,7 @@ export const POST = async (request: Request) => {
         }
 
         const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+        const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
 
         await db
         .update(videos)
@@ -79,6 +80,7 @@ export const POST = async (request: Request) => {
             muxPlaybackId: playbackId,
             muxAssetId: data.id,
             thumbnailUrl,
+            previewUrl,
         })
         .where(eq(videos.muxUploadId, data.upload_id));
         break;
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -61,7 +61,7 @@ const VideoSectionSuspense = () => {
                     <TableCell>
                       <div className="flex items-center gap-4">
                         <div className="relative aspect-video w-36 shrik-0">
-                          <VideoThumbnail imageUrl={video.thumbnailUrl} />
+                          <VideoThumbnail imageUrl={video.previewUrl} />
                         </div>
                       </div>
                     </TableCell>
