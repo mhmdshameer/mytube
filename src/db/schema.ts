95fe07fbb3e5963b99829fd0e9f9463c4d9be821
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-14 13:19:45 +0530

    schma change

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,17 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, uniqueIndex, uuid, integer } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  integer,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users",{
+export const users = pgTable(
+  "users",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
     clerkId: text("clerk_id").unique().notNull(),
     name: text("name").notNull(),
@@ -9,55 +19,68 @@ export const users = pgTable("users",{
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
-},(t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
 
-export const userRelations = relations(users, ({many}) => ({
-    videos: many(videos),
-}))
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
 
-export const categories = pgTable("categories",{
+export const categories = pgTable(
+  "categories",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull().unique(),
     description: text("description"),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
-},(t) => [uniqueIndex("name_idx").on(t.name)]);
+  },
+  (t) => [uniqueIndex("name_idx").on(t.name)]
+);
 
-export const categoryRelations = relations(categories, ({many}) => ({
-    videos: many(videos),
-}))
+export const categoryRelations = relations(categories, ({ many }) => ({
+  videos: many(videos),
+}));
 
-export const videos = pgTable("videos",{
-    id: uuid("id").primaryKey().defaultRandom(),
-    title: text("title").notNull(),
-    description: text("description"),
-    muxStatus: text("mux_status"),
-    muxAssetId: text("mux_asset_id").unique(),
-    muxUploadId: text("mux_upload_id").unique(),
-    muxPlaybackId: text("mux_playback_id"),
-    muxTrackId: text("mux_track_id").unique(),
-    muxTrackStatus: text("mux_track_status"),
-    thumbnailUrl: text("thumbnail_url"),
-    previewUrl: text("preview_url"),
-    duration: integer("duration"),
-    userId: uuid("user_id").references(() => users.id, {
-        onDelete: "cascade",
-    }).notNull(),
-    categoryId: uuid("category_id").references(() => categories.id,{
-        onDelete: "set null",
-    }),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull(),
-})
+export const videoVisibility = pgEnum("video_visibility", [
+  "private",
+  "public",
+]);
 
-export const videoRelations = relations(videos, ({one}) => ({
-    user: one(users,{
-        fields: [videos.userId],
-        references: [users.id],
-    }),
-    category: one(categories,{
-        fields: [videos.categoryId],
-        references: [categories.id],
-    }),
-}))
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  description: text("description"),
+  muxStatus: text("mux_status"),
+  muxAssetId: text("mux_asset_id").unique(),
+  muxUploadId: text("mux_upload_id").unique(),
+  muxPlaybackId: text("mux_playback_id"),
+  muxTrackId: text("mux_track_id").unique(),
+  muxTrackStatus: text("mux_track_status"),
+  thumbnailUrl: text("thumbnail_url"),
+  previewUrl: text("preview_url"),
+  duration: integer("duration").default(0).notNull(),
+  visibility: videoVisibility("visibility").default("private").notNull(),
+  userId: uuid("user_id")
+    .references(() => users.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: uuid("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
 
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}));
