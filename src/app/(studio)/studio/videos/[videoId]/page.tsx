7d5f5b355fef3a7a244dedec4c2358b84d665a39
Muhammed Shameer <mhmdshameerpm@gmail.com>
2025-05-22 14:53:11 +0530

    Get video

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -1,4 +1,12 @@
- const Page = () => {
+ export const dynamic = "force-dynamic";
+
+ interface PageProps {
+    params: Promise<{videoId: string}>;
+ };
+ 
+ const Page = async ({params}: PageProps) => {
+
+    const videoId = await params;
     return (
         <div>
             Video Id
